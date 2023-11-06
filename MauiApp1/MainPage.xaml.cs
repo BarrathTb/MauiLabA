902b4b63a0dc64874dac73437e41c287a37bc3d8
thomasbarrath <thomas.barrath@lanex.com>
2023-11-06 14:36:43 -0600

    Add project files.

---
+++ MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            if (int.TryParse(StartNum.Text, out int inputValue))
+            {
+                count = inputValue * 7;
+
+                if (count == 1)
+                    Output.Text = $"Clicked {count} time";
+                else
+                    Output.Text = $"Clicked {count} times";
+
+                SemanticScreenReader.Announce(CounterBtn.Text);
+            }
+            else
+            {
+                // Handle invalid input, e.g., show an error message
+                Output.Text = "Invalid input. Please enter a number.";
+            }
+        }
+    }
+}
